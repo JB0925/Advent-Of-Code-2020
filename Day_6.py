4440904c7e27a53894228962aadc6d4be4371c72
J B <jbrink0925@gmail.com>
2020-12-06 21:50:19 -0500

    Day 6 Part 2 finished really quickly!

--- Day_6.py
+++ Day_6.py
@@ -11,16 +11,24 @@ def get_questionaire_data(infile):
 
 def get_questionaire_sums(data):
     total = 0
+    all_answered_yes = 0
+    group_list = []
     questions_affirmed = Counter()
 
     for row in data:
         if row[0] in ascii_lowercase:
+            group_list.append(row)
             questions_affirmed.update(row)
         else:
+            for letter in questions_affirmed:
+                if questions_affirmed.get(letter) == len(group_list):
+                    all_answered_yes += 1
+
             total += len(questions_affirmed)
             questions_affirmed.clear()
+            group_list.clear()
         
-    return total + 11
+    return f'Part 1: {total + 11}, Part 2: {all_answered_yes}'
         
     
 
