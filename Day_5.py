0efc30d54bb59d09fd5298d17becbdf90176b925
J B <jbrink0925@gmail.com>
2020-12-06 20:06:52 -0500

    completed both parts of Day 5 AOC

--- Day_5.py
+++ Day_5.py
@@ -46,7 +46,33 @@ def get_highest_seat_number(infile):
     return max(seat_numbers)
 
 
+
+def get_my_seat_number(infile):
+    seat_numbers = []
+    my_seat = 0
+
+    with open(infile) as f:
+        reader = f.readlines()
+        for row in reader:
+            row_num = get_row(row)
+            col_num = get_column(row)
+            seat_num = row_num * 8 + col_num
+            seat_numbers.append(seat_num)
+
+    seat_numbers.sort()
+
+    for i in range(len(seat_numbers)-1):
+        sn = seat_numbers[i]
+        if seat_numbers.index(sn) == 0:
+            pass
+        else:
+            if sn - seat_numbers[i-1] == 2:
+                my_seat = sn - 1
+    return my_seat
+
+
 print(get_highest_seat_number(filename))
+print(get_my_seat_number(filename))
 
     
 
